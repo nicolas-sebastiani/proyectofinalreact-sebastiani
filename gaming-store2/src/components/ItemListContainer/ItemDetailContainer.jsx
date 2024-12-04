f6d22b8b5c708a9d83dc3a27ead96785f469b472
nicolas <nicosebastiani90@gmail.com>
2024-12-04 18:19:34 -0300

    loader itemdetail

--- gaming-store2/src/components/ItemListContainer/ItemDetailContainer.jsx
+++ gaming-store2/src/components/ItemListContainer/ItemDetailContainer.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Loader from "../Loaders/Loader.jsx"
 import ItemDetail from './ItemDetail.jsx'
 import { db } from '../../services/firebase/index.js'
 import { doc, getDoc } from 'firebase/firestore'
@@ -7,22 +8,25 @@ import { doc, getDoc } from 'firebase/firestore'
 const ItemDetailContainer = () => {
 
     const [item, setItem] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
     const id = useParams().id;
 
     useEffect(() => {
-        // const docRef = doc(db, "productos", id);
+        setIsLoading(true);
         getDoc(doc(db, "products", id))
             .then((resp) => {
-                // console.log(resp.price)
-                    setItem(
+                setItem(
                         { ...resp.data(), id: resp.id }
                     )
                 })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }, [id])
     
     return (
         <div>
-            {item && <ItemDetail item={item}/>}
+            {isLoading ? <Loader/> : <ItemDetail item={item}/>}
         </div>
     )
 }
